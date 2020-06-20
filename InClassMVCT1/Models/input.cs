30d1d05a4b6109475823d8a580ba667af815f1d0
Sutama Mitra <sutama.mitra@outlook.com>
2020-06-20 16:47:29 -0400

    Add project files.

---
+++ InClassMVCT1/Models/input.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InClassMVCT1.Models
+{
+    public class input
+    {
+        public string name { get; set; }
+        public string state { get; set; }
+    }
+}
